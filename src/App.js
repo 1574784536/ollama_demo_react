1a47a68038a9cadfd5fb72070fdeb6a4e54a45dd
xudongyang <17520275940@163.com>
2024-08-27 00:51:47 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+    const [chatBox, setCheckBox] = useState([]);
+    const [inputText, setInputText] = useState([]);
+
+    const sendMessage = () => {
+        if (!inputText.length) return;
+        createMessage(inputText);
+    }
+
+    async function createMessage(text) {
+        const msgMe = {
+            name: 'Me',
+            text,
+            dateTime: new Date().toString()
+        }
+
+        const msgAi = {
+            name: 'AI',
+            text: '...',
+            dateTime: '...'
+        }
+
+        setCheckBox([...chatBox, msgMe, msgAi]);
+
+        const {data} = await axios.post('http://localhost:8081/ollamaChat/chat', {
+            'message': text
+        });
+        setCheckBox(chatBox=>{
+            const lastMsg = chatBox[chatBox.length-1];
+            lastMsg.text = data.message;
+            lastMsg.dateTime = new Date().toString();
+
+            return [...chatBox];
+        });
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          {
+              chatBox.map(msg => (
+                  <div key={msg.text}>
+                      <p>{msg.name}:{msg.dateTime}</p>
+                      <p>{msg.text}</p>
+                      <hr/>
+                  </div>
+              ))
+          }
+      </div>
+        <div>
+            <input type='text' onInput={(e) => setInputText(e.target.value)}/>
+            <button onClick={sendMessage}>发送</button>
+        </div>
     </div>
   );
 }
